098b94f10cec640a1d4f79679266b21f6c519c61
asaldivar <augustosaldivar@assertsolutions.com>
2025-06-06 09:36:23 -0300

    allow printing app version

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { version } from '../package.json';
 
 async function bootstrap() {
+  console.log(`app version: ${version}`);
   const app = await NestFactory.create(AppModule);
   app.enableShutdownHooks(); // recursos ?
   await app.listen(process.env.PORT ?? 3000);
